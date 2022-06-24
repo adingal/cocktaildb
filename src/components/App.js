bfeaf899547ad5321b3d81878b0b684b8ec38fad
Alex Dingal <alex@emarketmed.com>
2022-06-24 08:59:27 +0800

    [Update] Integrate redux, handlers and states

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { CssBaseline } from '@mui/material'
 import Layout from 'components/Layout'
 import Header from 'components/Header'
+import Content from 'components/Content'
 import Footer from 'components/Footer'
 
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchCocktail } from 'store/api'
+
 const App = () => {
+  const [searchValue, setSearchValue] = useState('vodka')
+  const drinks = useSelector((state) => state.cocktails.drinks)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    // Debounce for 500ms
+    const timeOutId = window.setTimeout(() => {
+      dispatch(fetchCocktail(searchValue))
+    }, 500)
+    return () => {
+      window.clearTimeout(timeOutId)
+    }
+  }, [searchValue])
+
   return (
     <>
       <CssBaseline />
       <Layout>
-        <Header />
+        <Header onInputChange={setSearchValue} />
+        <Content drinks={drinks} />
         <Footer />
       </Layout>
     </>
