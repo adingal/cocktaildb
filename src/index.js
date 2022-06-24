fa4f7119bd61c37eee628acdab8be7c7f35afff4
Alex Dingal <alex@emarketmed.com>
2022-06-24 09:01:26 +0800

    [Update] Configure redux store and middlewares

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from 'components/App'
 
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { reducers } from 'store/reducers'
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+
+const logger = createLogger()
+const store = createStore(reducers, applyMiddleware(thunk, logger))
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
